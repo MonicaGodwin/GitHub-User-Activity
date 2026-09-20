7b096e1f6ba94d9470efe957f4685fd45487c626
MonicaGodwin <monicagodwinn@gmail.com>
2026-09-21 00:00:56 +0100

    my commit

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ raw_data = raw_data.decode()
 
 converted_data = json.loads(raw_data)
 
-for converted in converted_data:
+content_file = []
     
-    if not os.path.exists("file.json"):
-        content = []
-        with open("file.json", "w") as file:
-            json.dump(content, file, indent=4)
-    else:
-        with open("file.json", "r") as data:
-            file_content = json.loads(data)
+if not os.path.exists("file.json"):        
+    with open("file.json", "w") as file:
+        json.dump(content_file, file, indent=4)
+else:
+    with open("file.json", "r") as data:
+        file_content = json.load(data)
+
+for converted in converted_data:
+    content = f"{converted['id']} - {converted['repo']['name']}"
+    with open("file.json", "w") as data_file:
+        json.dump(content, data_file, indent=4)
