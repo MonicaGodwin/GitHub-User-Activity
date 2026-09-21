72b595c49cdb35a74305f5503a523c22f0b56a38
MonicaGodwin <monicagodwinn@gmail.com>
2026-09-21 12:02:37 +0100

    my commit

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # GitHub-User-Activity
+A simple CLI built with python standard library. 
+
+# Requirements
+
+* python3
+* No external library included.
+
+# Getting Started
+
+1. clone the repository
+
+https://github.com/MonicaGodwin/GitHub-User-Activity.git
+
+2. Navigate into the project directory
+
+cd GitHub-User-Activity
+
+# Usage
+
+The command structure is:
+ 
+python3 main.py <github username>
+
+# User's Data
+
+The users data is stored in file.json with the following information:
+
+{
+    "type": "PushEvent"
+    "repo": {
+        "name" : "username/repo"
+    }
+    "created_at" : "time"
+}
+
+
+# Project structure
+
+github-user-activity/
+│__file.json
+│__main.py
+│__README.md
+
+
+# Project page url
+
+https://roadmap.sh/projects/github-user-activity
+
+# License
+
+This project is open source and available for learning and personal use.
+
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from urllib.request import urlopen
 import json
 import os
 
-
 if len(sys.argv) < 2:
     print("Usage: [main.py] [username]")
     sys.exit()
@@ -12,19 +11,13 @@ elif len(sys.argv) > 2:
     sys.exit()
 
 user_input = sys.argv[1]
-
 url = f"https://api.github.com/users/{user_input}/events"
-
 response = urlopen(url)
-
 raw_data = response.read()
-
 raw_data = raw_data.decode()
-
 converted_data = json.loads(raw_data)
 
-content_file = []
-    
+content_file = []   
 if not os.path.exists("file.json"):        
     with open("file.json", "w") as file:
         json.dump(content_file, file, indent=4)
@@ -32,10 +25,34 @@ else:
     with open("file.json", "r") as data:
         file_content = json.load(data)
 
-
 for converted in converted_data:
-    content = f"{converted['type']} - {converted['repo']['name']} - {converted['created_at']}"
+    description = ""
+    event_type = converted["type"]
+    if event_type == "PushEvent":
+        description = "Pushed commit"
+    elif event_type == "CreateEvent":
+        description = "Created a repository"
+    elif event_type == "WatchEvent":
+        description = "Starred a repository"
+    elif event_type == "IssuesEvent":
+        description = "updated an issue"
+    elif event_type == "ForkEvent":
+        description = "Forked a repository"
+    elif event_type == "DeleteEvent":
+        description = "Deleted a branch"
+    else:
+        description = "Unknown event"
+    repo = converted["repo"]["name"]
+    time = converted["created_at"]
+
+    content = {
+        "type": event_type,
+        "description": description,
+        "repo": repo,
+        "created_at": time
+    }
     content_file.append(content)
+    print(f"{content["description"]} to {content["repo"]}\n{content['created_at']}\n")
 
 with open("file.json", "w") as data_file:
     json.dump(content_file, data_file, indent=4)
